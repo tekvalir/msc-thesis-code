a469f782a96c24c00dc47e6ca99b170679d00a6e
tekvalir <tekvalir@gmail.com>
2026-09-08 16:19:41 +0200

    save stats and improve gitignore

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,8 +1,8 @@
 import os
 import subprocess
-import re
 import shutil
 import time
+import csv
 
 TIMEOUT = 3600
 
@@ -31,6 +31,7 @@ def make_qif_cmd(folder: str, filename: str):
     return [os.path.join(ABACUS, QIF_PATH), f"{INST_OUT}", "-f", f"{FUNC_OUT}", "-d", os.path.join(PWD, BIN_FOLDER, folder, filename), "-o", "result.txt"]
 
 def analyse_file(folder: str, filename: str, summary):
+    last_folder = folder.split("/")[-1]
     loc_summary = {}
     print(f"=> Processing {os.path.join(folder, filename)}")
     pin_cmd = make_pin_cmd(folder, filename)
@@ -40,6 +41,8 @@ def analyse_file(folder: str, filename: str, summary):
         print(" ".join(pin_cmd))
         print("==> QIF command")
         print(" ".join(qif_cmd))
+        loc_summary["pin-cmd"] = pin_cmd
+        loc_summary["qif-cmd"] = qif_cmd
     else:
         print("==> Running pin...")
         start = time.time()
@@ -67,19 +70,18 @@ def analyse_file(folder: str, filename: str, summary):
             loc_summary["abacus_status"] = 124
             loc_summary["abacus_time"] = delay
             print("====> QIF timeout")
-            shutil.move("result.txt",  os.path.join(PWD, OUTPUT_FOLDER, f"{last_folder}-{filename}-res.txt"))
         else:
             delay = time.time() - start
             loc_summary["abacus_status"] = proc.returncode
             loc_summary["abacus_time"] = delay
+            shutil.move("result.txt",  os.path.join(PWD, OUTPUT_FOLDER, f"{last_folder}-{filename}-res.txt"))
 
-        if DEBUG:
-            print(loc_summary)
-
-        last_folder = folder.split("/")[-1]
-        summary[f"{last_folder}-{filename}"] = loc_summary
         shutil.move(f"{FUNC_OUT}", f"{OUTPUT_FOLDER}/{last_folder}-{filename}-func.txt")
         shutil.move(f"{INST_OUT}", f"{OUTPUT_FOLDER}/{last_folder}-{filename}-inst.txt")
+
+    if DEBUG:
+        print(loc_summary)
+    summary[f"{last_folder}-{filename}"] = loc_summary
 
 if __name__ == "__main__":
     summary = {}
@@ -89,4 +91,11 @@ if __name__ == "__main__":
             for bin_file in os.listdir(fp):
                 analyse_file(folder, bin_file, summary)
 
-    print(summary)
+    with open(f"{OUTPUT_FOLDER}/summary.csv", 'w', newline='') as f:
+        fields = ["name"] + list(summary[list(summary)[0]])
+        writer = csv.DictWriter(f, fieldnames=fields)
+        writer.writeheader()
+        for name in list(summary):
+            row = summary[name]
+            row["name"] = name
+            writer.writerow(row)
